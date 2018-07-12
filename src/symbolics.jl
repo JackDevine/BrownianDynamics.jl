eb84be052d24735e339c90a619e69198e305bcfc
Jack Devine <devja964@student.otago.ac.nz>
2018-07-13 09:50:15 +1200

    Use metaprogramming to create the Jacobian functions

--- src/symbolics.jl
+++ src/symbolics.jl
@@ -1,8 +1,6 @@
 #=
-This script will create code for the Jacobian of the finite volume discretization. If you
-run this script, then the code which is commented out at the bottom of the script will be
-placed into your clipboard. The code can then be used to calculate Jacobians. We have four
-functions that get created,
+This script will create code for the Jacobian of the finite volume discretization. The
+code is used to calculate Jacobians. We have four functions that get created:
 density_flux!             The density part of the flux differentiated w.r.t. P_ij
 density_coupling!         The density part of the flux differentiated w.r.t. T_ij
 temperature_coupling!     The temperature part of the flux differentiated w.r.t. P_ij
@@ -110,162 +108,39 @@ function_names = ["density_flux!","density_coupling!",
                   "temperature_coupling!","temperature_flux!"]
 stencils = [density_stencil,density_coupling_stencil,
             temperature_coupling_stencil,temperature_stencil]
-code = Array{String}(4)
 for i in 1:4
-    function_name = function_names[i]
-    stencil = stencils[i]
-    code[i] =
-    """
-    function $(function_name)(::Type{Val{:jac}},jac,u,params)
-        @unpack Pmat,Tmat,Jx,Jy,V,Vxshift,Vyshift,V_x,V_y,dx,dy,A,coupling = params
-        nx = length(indices(Tmat)[1])-2
-        ny = length(indices(Tmat)[2])-2
-        Pmat[1:nx,1:ny] .= reshape(u[1:nx*ny],nx,ny)
-        # Periodicity in the x direction.
-        Pmat[0,1:ny] .= Pmat[nx-1,1:ny]
-        Pmat[nx+1,1:ny] .= Pmat[2,1:ny]
+    function_name = parse(function_names[i])
+    stencil = parse.(stencils[i])
 
-        Tmat[1:nx,1:ny] .= reshape(u[(nx*ny+1):end],nx,ny)
-        # Periodicity in the x direction.
-        Tmat[0,1:ny] .= Tmat[nx-1,1:ny]
-        Tmat[nx+1,1:ny] .= Tmat[2,1:ny]
+    quote
+        function ($function_name)(::Type{Val{:jac}},jac,u,params)
+            @unpack Pmat,Tmat,Jx,Jy,V,Vxshift,Vyshift,V_x,V_y,dx,dy,A,coupling = params
+            nx = length(indices(Tmat)[1])-2
+            ny = length(indices(Tmat)[2])-2
+            Pmat[1:nx,1:ny] .= reshape(u[1:nx*ny],nx,ny)
+            # Periodicity in the x direction.
+            Pmat[0,1:ny] .= Pmat[nx-1,1:ny]
+            Pmat[nx+1,1:ny] .= Pmat[2,1:ny]
 
-        inds = Array{Int64}(5)
-        for row in 1:nx*ny
-            i,j = ind2sub((nx,ny),row)
-            stencil_indices!(inds,(nx,ny),row)
-            jac[row,inds] .= [$(stencil[1]),$(stencil[2]),$(stencil[3]),
-                              $(stencil[4]),$(stencil[5])]
+            Tmat[1:nx,1:ny] .= reshape(u[(nx*ny+1):end],nx,ny)
+            # Periodicity in the x direction.
+            Tmat[0,1:ny] .= Tmat[nx-1,1:ny]
+            Tmat[nx+1,1:ny] .= Tmat[2,1:ny]
+
+            inds = Array{Int64}(5)
+            for row in 1:nx*ny
+                i,j = ind2sub((nx,ny),row)
+                stencil_indices!(inds,(nx,ny),row)
+                jac[row,inds] .= [$(stencil[1]),$(stencil[2]),$(stencil[3]),
+                                  $(stencil[4]),$(stencil[5])]
+            end
+            jac[diagind(jac,nx*ny-1)] = zero(eltype(jac))
+            jac[diagind(jac,-nx*ny+1)] = zero(eltype(jac))
+            jac[diagind(jac,nx*ny-nx)] = zero(eltype(jac))
+            jac[diagind(jac,-nx*ny+nx)] = zero(eltype(jac))
+            jac
         end
-        jac[diagind(jac,nx*ny-1)] = zero(eltype(jac))
-        jac[diagind(jac,-nx*ny+1)] = zero(eltype(jac))
-        jac[diagind(jac,nx*ny-nx)] = zero(eltype(jac))
-        jac[diagind(jac,-nx*ny+nx)] = zero(eltype(jac))
-        jac
-    end
+    end |> eval
 
-    $(function_name)(::Type{Val{:jac}},jac,u,params,t) = $(function_name)(Val{:jac},jac,u,params)
-    """
+    eval(:(($function_name)(::Type{Val{:jac}},jac,u,params,t) = ($function_name)(Val{:jac},jac,u,params)))
 end
-
-join(code,"\n") |> clipboard
-## Results.
-# function density_flux!(::Type{Val{:jac}},jac,u,params)
-#     @unpack Pmat,Tmat,Jx,Jy,V,Vxshift,Vyshift,V_x,V_y,dx,dy,A,coupling = params
-#     ny = length(indices(Tmat)[1])-2
-#     nx = length(indices(Tmat)[2])-2
-#     Pmat[1:nx,1:ny] .= reshape(u[1:nx*ny],nx,ny)
-#     # Periodicity in the x direction.
-#     Pmat[0,1:ny] .= Pmat[nx-1,1:ny]
-#     Pmat[nx+1,1:ny] .= Pmat[2,1:ny]
-#
-#     Tmat[1:nx,1:ny] .= reshape(u[(nx*ny+1):end],nx,ny)
-#     # Periodicity in the x direction.
-#     Tmat[0,1:ny] .= Tmat[nx-1,1:ny]
-#     Tmat[nx+1,1:ny] .= Tmat[2,1:ny]
-#
-#     inds = Array{Int64}(5)
-#     for row in 1:nx*ny
-#         i,j = ind2sub((nx,ny),row)
-#         stencil_indices!(inds,(nx,ny),row)
-#         jac[row,inds] .= [-(Tmat[i-1,j]+Tmat[i,j-1]+4*Tmat[i,j]+Tmat[i,j+1]+Tmat[i+1,j]+dx*(V_x[i,j]-V_x[i+1,j])+dy*(V_y[i,j]-V_y[i,j+1]))/(2*dx*dy),(Tmat[i-1,j]+Tmat[i,j]-V_x[i,j]*dx)/(2*dx*dy),(Tmat[i,j-1]+Tmat[i,j]-V_y[i,j]*dy)/(2*dx*dy),
-#                           (Tmat[i,j]+Tmat[i+1,j]+V_x[i+1,j]*dx)/(2*dx*dy),(Tmat[i,j]+Tmat[i,j+1]+V_y[i,j+1]*dy)/(2*dx*dy)]
-#     end
-#     jac[diagind(jac,nx*ny-1)] = zero(eltype(jac))
-#     jac[diagind(jac,-nx*ny+1)] = zero(eltype(jac))
-#     jac[diagind(jac,nx*ny-nx)] = zero(eltype(jac))
-#     jac[diagind(jac,-nx*ny+nx)] = zero(eltype(jac))
-#     jac
-# end
-#
-# density_flux!(::Type{Val{:jac}},jac,u,params,t) = density_flux!(Val{:jac},jac,u,params)
-#
-# function density_coupling!(::Type{Val{:jac}},jac,u,params)
-#     @unpack Pmat,Tmat,Jx,Jy,V,Vxshift,Vyshift,V_x,V_y,dx,dy,A,coupling = params
-#     ny = length(indices(Tmat)[1])-2
-#     nx = length(indices(Tmat)[2])-2
-#     Pmat[1:nx,1:ny] .= reshape(u[1:nx*ny],nx,ny)
-#     # Periodicity in the x direction.
-#     Pmat[0,1:ny] .= Pmat[nx-1,1:ny]
-#     Pmat[nx+1,1:ny] .= Pmat[2,1:ny]
-#
-#     Tmat[1:nx,1:ny] .= reshape(u[(nx*ny+1):end],nx,ny)
-#     # Periodicity in the x direction.
-#     Tmat[0,1:ny] .= Tmat[nx-1,1:ny]
-#     Tmat[nx+1,1:ny] .= Tmat[2,1:ny]
-#
-#     inds = Array{Int64}(5)
-#     for row in 1:nx*ny
-#         i,j = ind2sub((nx,ny),row)
-#         stencil_indices!(inds,(nx,ny),row)
-#         jac[row,inds] .= [(Pmat[i-1,j]+Pmat[i,j-1]-4*Pmat[i,j]+Pmat[i,j+1]+Pmat[i+1,j])/(2*dx*dy),(Pmat[i-1,j]-Pmat[i,j])/(2*dx*dy),(Pmat[i,j-1]-Pmat[i,j])/(2*dx*dy),
-#                           (-Pmat[i,j]+Pmat[i+1,j])/(2*dx*dy),(-Pmat[i,j]+Pmat[i,j+1])/(2*dx*dy)]
-#     end
-#     jac[diagind(jac,nx*ny-1)] = zero(eltype(jac))
-#     jac[diagind(jac,-nx*ny+1)] = zero(eltype(jac))
-#     jac[diagind(jac,nx*ny-nx)] = zero(eltype(jac))
-#     jac[diagind(jac,-nx*ny+nx)] = zero(eltype(jac))
-#     jac
-# end
-#
-# density_coupling!(::Type{Val{:jac}},jac,u,params,t) = density_coupling!(Val{:jac},jac,u,params)
-#
-# function temperature_coupling!(::Type{Val{:jac}},jac,u,params)
-#     @unpack Pmat,Tmat,Jx,Jy,V,Vxshift,Vyshift,V_x,V_y,dx,dy,A,coupling = params
-#     ny = length(indices(Tmat)[1])-2
-#     nx = length(indices(Tmat)[2])-2
-#     Pmat[1:nx,1:ny] .= reshape(u[1:nx*ny],nx,ny)
-#     # Periodicity in the x direction.
-#     Pmat[0,1:ny] .= Pmat[nx-1,1:ny]
-#     Pmat[nx+1,1:ny] .= Pmat[2,1:ny]
-#
-#     Tmat[1:nx,1:ny] .= reshape(u[(nx*ny+1):end],nx,ny)
-#     # Periodicity in the x direction.
-#     Tmat[0,1:ny] .= Tmat[nx-1,1:ny]
-#     Tmat[nx+1,1:ny] .= Tmat[2,1:ny]
-#
-#     inds = Array{Int64}(5)
-#     for row in 1:nx*ny
-#         i,j = ind2sub((nx,ny),row)
-#         stencil_indices!(inds,(nx,ny),row)
-#         jac[row,inds] .= [-A*(-V[i,j]*(Tmat[i-1,j]+Tmat[i,j-1]+4*Tmat[i,j]+Tmat[i,j+1]+Tmat[i+1,j]+dx*(V_x[i,j]-V_x[i+1,j])+dy*(V_y[i,j]-V_y[i,j+1]))+Vxshift[i,j]*(Tmat[i-1,j]+Tmat[i,j]+V_x[i,j]*dx)+Vxshift[i+1,j]*(Tmat[i,j]+Tmat[i+1,j]-V_x[i+1,j]*dx)+Vyshift[i,j]*(Tmat[i,j-1]+Tmat[i,j]+V_y[i,j]*dy)+Vyshift[i,j+1]*(Tmat[i,j]+Tmat[i,j+1]-V_y[i,j+1]*dy))/(2*dx*dy),-A*(V[i,j]-Vxshift[i,j])*(Tmat[i-1,j]+Tmat[i,j]-V_x[i,j]*dx)/(2*dx*dy),-A*(V[i,j]-Vyshift[i,j])*(Tmat[i,j-1]+Tmat[i,j]-V_y[i,j]*dy)/(2*dx*dy),
-#                           -A*(V[i,j]-Vxshift[i+1,j])*(Tmat[i,j]+Tmat[i+1,j]+V_x[i+1,j]*dx)/(2*dx*dy),-A*(V[i,j]-Vyshift[i,j+1])*(Tmat[i,j]+Tmat[i,j+1]+V_y[i,j+1]*dy)/(2*dx*dy)]
-#     end
-#     jac[diagind(jac,nx*ny-1)] = zero(eltype(jac))
-#     jac[diagind(jac,-nx*ny+1)] = zero(eltype(jac))
-#     jac[diagind(jac,nx*ny-nx)] = zero(eltype(jac))
-#     jac[diagind(jac,-nx*ny+nx)] = zero(eltype(jac))
-#     jac
-# end
-#
-# temperature_coupling!(::Type{Val{:jac}},jac,u,params,t) = temperature_coupling!(Val{:jac},jac,u,params)
-#
-# function temperature_flux!(::Type{Val{:jac}},jac,u,params)
-#     @unpack Pmat,Tmat,Jx,Jy,V,Vxshift,Vyshift,V_x,V_y,dx,dy,A,coupling = params
-#     ny = length(indices(Tmat)[1])-2
-#     nx = length(indices(Tmat)[2])-2
-#     Pmat[1:nx,1:ny] .= reshape(u[1:nx*ny],nx,ny)
-#     # Periodicity in the x direction.
-#     Pmat[0,1:ny] .= Pmat[nx-1,1:ny]
-#     Pmat[nx+1,1:ny] .= Pmat[2,1:ny]
-#
-#     Tmat[1:nx,1:ny] .= reshape(u[(nx*ny+1):end],nx,ny)
-#     # Periodicity in the x direction.
-#     Tmat[0,1:ny] .= Tmat[nx-1,1:ny]
-#     Tmat[nx+1,1:ny] .= Tmat[2,1:ny]
-#
-#     inds = Array{Int64}(5)
-#     for row in 1:nx*ny
-#         i,j = ind2sub((nx,ny),row)
-#         stencil_indices!(inds,(nx,ny),row)
-#         jac[row,inds] .= [-A*(8*coupling+V[i,j]*(Pmat[i-1,j]+Pmat[i,j-1]-4*Pmat[i,j]+Pmat[i,j+1]+Pmat[i+1,j])-Vxshift[i,j]*(Pmat[i-1,j]-Pmat[i,j])+Vxshift[i+1,j]*(Pmat[i,j]-Pmat[i+1,j])-Vyshift[i,j]*(Pmat[i,j-1]-Pmat[i,j])+Vyshift[i,j+1]*(Pmat[i,j]-Pmat[i,j+1]))/(2*dx*dy),A*(2*coupling-V[i,j]*(Pmat[i-1,j]-Pmat[i,j])+Vxshift[i,j]*(Pmat[i-1,j]-Pmat[i,j]))/(2*dx*dy),A*(2*coupling-V[i,j]*(Pmat[i,j-1]-Pmat[i,j])+Vyshift[i,j]*(Pmat[i,j-1]-Pmat[i,j]))/(2*dx*dy),
-#                           A*(2*coupling+V[i,j]*(Pmat[i,j]-Pmat[i+1,j])-Vxshift[i+1,j]*(Pmat[i,j]-Pmat[i+1,j]))/(2*dx*dy),A*(2*coupling+V[i,j]*(Pmat[i,j]-Pmat[i,j+1])-Vyshift[i,j+1]*(Pmat[i,j]-Pmat[i,j+1]))/(2*dx*dy)]
-#     end
-#     jac[diagind(jac,nx*ny-1)] = zero(eltype(jac))
-#     jac[diagind(jac,-nx*ny+1)] = zero(eltype(jac))
-#     jac[diagind(jac,nx*ny-nx)] = zero(eltype(jac))
-#     jac[diagind(jac,-nx*ny+nx)] = zero(eltype(jac))
-#     jac
-# end
-#
-# temperature_flux!(::Type{Val{:jac}},jac,u,params,t) = temperature_flux!(Val{:jac},jac,u,params)
